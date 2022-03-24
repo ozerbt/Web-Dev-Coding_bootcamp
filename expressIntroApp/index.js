8f5fdd3be9c15110938d9d4c9994e13e04ae6820
ozerbt <54027925+ozerbt@users.noreply.github.com>
2022-03-24 11:38:05 -0400

    path params + query strings for express

--- expressIntroApp/index.js
+++ expressIntroApp/index.js
@@ -7,6 +7,26 @@ const app = express()
 //    //res.send("HELLO WE GOUT YOUR REQUEST THIS IS A RESPONSE")
 //    res.send('<h1>This is my webpage!</h1>')
 //})
+
+app.get('/r/:subreddit/:postId', (req, res) => {
+    const { subreddit, postId } = req.params
+    console.log(subreddit, postId)
+    res.send(`<h1>Viewing Post ID: ${postId} on the ${subreddit} subreddit</h1>`)
+})
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params
+    console.log(subreddit)
+    res.send(`<h1>you are on the ${subreddit} subreddit</h1>`)
+})
+
+app.get('/search', (req, res) => {
+    const { q } = req.query;
+    if (!q) {
+        res.send(`<h1> Nothing found if nothing searched</h1>`)
+    }
+    res.send(`<h1> Search results for ${q}</h1>`)
+})
+
 app.post('/cats', (req, res) => {
     res.send('POST REQUEST TO CATS THIS IS A DIFFERENT RESPONSE')
 })
@@ -31,6 +51,8 @@ app.get('*', (req, res) => {
 // / cats => 'meow'
 // /dogs => 'wolf'
 // '/' => home page
+
+// /r/SOMETHINGHERE
 
 
 
